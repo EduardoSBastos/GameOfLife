904fb7094b7e59c7eb132177c7eb424e398906d0
Eduardo Bastos <eduardosbastos@outlook.com>
2022-11-13 23:20:30 -0300

    Add esc handling and add update cycle

--- Game/game.py
+++ Game/game.py
@@ -1,5 +1,8 @@
+import sys
 import numpy as np
 import time
+import pygame
+from pygame.locals import *
 
 from Grid import Board
 from Visual import Visual
@@ -12,12 +15,25 @@ board.raise_cell(5,3)
 print(board.grid)
 visual = Visual(board.size, (15,15))
 visual.update(board.grid)
-    
-quitted = False
-while True:
-    time.sleep(0.8)
 
-    board.step()
-    visual.update(board.grid)
-    print(board.grid)
+counter = 0
+DELTA_TIME = 1/60
+
+while True:
+    time.sleep(DELTA_TIME)
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT: 
+            sys.exit()
+        if event.type == KEYDOWN:
+            if event.key == K_KP_ENTER:
+                print("Enter")
+            if event.type == 768: #Esc
+                sys.exit()
+            print(event.type)
+
+    counter += 1 * DELTA_TIME
+    if counter >= 1:
+        board.step()
+        visual.update(board.grid)
+        counter = 0
     
