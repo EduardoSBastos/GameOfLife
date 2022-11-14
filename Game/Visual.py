2865d4cf9c861d3a45920a2fea2141f500b1323d
Eduardo Bastos <eduardosbastos@outlook.com>
2022-11-13 21:44:32 -0300

    Implement Visual Class

--- Game/Visual.py
+++ Game/Visual.py
@@ -1,7 +1,28 @@
+import sys, pygame
 
-from Game.Grid import Grid
+BLACK = 0, 0, 0
+WHITE = 255,255,255
+GREY = 60,60,60
+SCREEN_SIZE = (1280,800)
 
 class Visual:
-  def __init__(self, grid:Grid):
+  def __init__(self, grid_size, cell_size):
+    
+    self.grid_size = grid_size
+    self.cell_size = cell_size
+    self.innerScreenSize = (grid_size[0]*cell_size[0], grid_size[1]*cell_size[1])
 
-    return
+    pygame.init()
+    self.initialize_screen(self.innerScreenSize)
+
+  def initialize_screen(self, innerScreenSize):
+    self.screen = pygame.display.set_mode(SCREEN_SIZE)
+    self.screen.fill(GREY)
+    innerScreenRect = pygame.Rect(30,30, 30+innerScreenSize[0], 30+innerScreenSize[1])
+    pygame.draw.rect(self.screen, BLACK, innerScreenRect)
+    pygame.display.flip()
+
+  def update(self, grid):
+    self.screen.fill(GREY)
+    pygame.draw.rect(self.screen, WHITE, pygame.Rect(30,30,60,60))
+    pygame.display.flip()
--- Game/game.py
+++ Game/game.py
@@ -1,21 +1,25 @@
 import numpy as np
 
 from Grid import Board
+from Visual import Visual
 
-board = Board((20,10))
+board = Board((10,10))
 board.raise_cell(5,5)
 board.raise_cell(5,4)
 board.raise_cell(5,3)
 
 print(board.grid)
-
+visual = Visual(board.size, (10,10))
+visual.update(board.grid)
+    
 quitted = False
-
 while not quitted:
     inp = input()
-    if inp == "q": quitted = True
+    if inp == "q": 
+        quitted = True
+        continue
 
     board.step()
+    visual.update(board.grid)
     print(board.grid)
-
     
