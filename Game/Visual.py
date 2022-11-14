82aafd26c305f3f2d3fa6bba6e7a6fcda4843e3d
Eduardo Bastos <eduardosbastos@outlook.com>
2022-11-13 21:47:00 -0300

    Fix spacings

--- Game/Visual.py
+++ Game/Visual.py
@@ -11,9 +11,9 @@ class Visual:
     self.grid_size = grid_size
     self.cell_size = cell_size
     self.innerScreenSize = (grid_size[0]*cell_size[0], grid_size[1]*cell_size[1])
-
     pygame.init()
     self.initialize_screen(self.innerScreenSize)
+
 
   def initialize_screen(self, innerScreenSize):
     self.screen = pygame.display.set_mode(SCREEN_SIZE)
@@ -21,6 +21,7 @@ class Visual:
     innerScreenRect = pygame.Rect(30,30, 30+innerScreenSize[0], 30+innerScreenSize[1])
     pygame.draw.rect(self.screen, BLACK, innerScreenRect)
     pygame.display.flip()
+
 
   def update(self, grid):
     self.screen.fill(GREY)
