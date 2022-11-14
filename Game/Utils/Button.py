87f4c6b2b87e4c22a809e01ff78be06e6cdf45d7
Eduardo Bastos <eduardosbastos@outlook.com>
2022-11-14 14:26:23 -0300

    Add Button class

--- Game/Utils/Button.py
+++ Game/Utils/Button.py
@@ -2,16 +2,17 @@ import pygame, sys
 
 
 class Button():
-    def __init__(self, text, image, size, position):
+    def __init__(self, text, image, size, position, bg_color=None):
         self.text_input = text
         self.image = pygame.image.load(image)
         self.size = size
         self.pos = position
-        self.color = (0,255,255)
+        self.bg_color = bg_color
 
     def draw(self, surface):
         rect = pygame.Rect(self.pos[0], self.pos[1], self.size[0], self.size[1])
-        pygame.draw.rect(surface, self.color, rect)
+        if self.bg_color != None:
+            pygame.draw.rect(surface, self.bg_color, rect)
         self.image = pygame.transform.scale(self.image, self.size)
         surface.blit(self.image, rect)
 
