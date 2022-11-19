9ef13f28567e5f72037893444c2d128ffbe4a3a6
EddSB <eduardostimamiglio@gmail.com>
2022-11-18 21:40:34 -0300

    get mouse over button

--- Game/Utils/Button.py
+++ Game/Utils/Button.py
@@ -1,6 +1,5 @@
 import pygame, sys
 
-
 class Button():
     def __init__(self, text, image, size, position, bg_color=None):
         self.text_input = text
@@ -9,10 +8,17 @@ class Button():
         self.pos = position
         self.bg_color = bg_color
 
+        self.image = pygame.transform.scale(self.image, self.size)
+        self.rect = pygame.Rect(self.pos[0], self.pos[1], self.size[0], self.size[1])
+
+    def update(self):
+        
+        if self.rect.collidepoint(pygame.mouse.get_pos()):
+            print ('mouse is over Button')
+
     def draw(self, surface):
-        rect = pygame.Rect(self.pos[0], self.pos[1], self.size[0], self.size[1])
         if self.bg_color != None:
             pygame.draw.rect(surface, self.bg_color, rect)
-        self.image = pygame.transform.scale(self.image, self.size)
-        surface.blit(self.image, rect)
+        surface.blit(self.image, self.rect)
+
 
